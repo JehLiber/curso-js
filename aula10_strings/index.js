8da059cc8f6e60af59b1d54fd981a02064800d9f
Jessica Liberato <100487905+JehLiber@users.noreply.github.com>
2025-11-18 16:02:02 -0300

    sobre Strings
    Atualização do curso js, falando um pouco mais sobre strings

--- aula10_strings/index.js
+++ aula10_strings/index.js
@@ -1,4 +1,20 @@
-let umaString = " Um \"texto\""; // para as "" aparecerem \ caracatere de escape
+// let umaString = " Um \"texto\""; // para as "" aparecerem \ caracatere de escape
+//               01234567   
+let umaString = "Um texto";
 
-console.log(umaString);
+console.log(umaString.charAt(5)); /*Para saber a posição do determinado elemento*/ 
+console.log(umaString.lastIndexOf('m', 4)); /*de tras para frente*/
+console.log(umaString.match(/[a-z]/g)); //expressão regular
+console.log(umaString.search(/x/)); //para procurar.
+console.log(umaString.replace('Um', 'Outra')) //ele subustituiu a string 'Um' para 'Outra'
+console.log(umaString.replace(/t/g, '#')); //subistituiu os 't' por '#'
+console.log(umaString.length); //saber o tamanho da string = 8 por que começa do 0
+console.log(umaString.slice(2, 5)); //mostra o inicio e o final que você deseja
+console.log(umaString.split(' ', 2));
+console.log(umaString.toUpperCase());
+console.log(umaString.toLowerCase());
 
+
+console.log(umaString.concat(' em um lindo dia.'));
+console.log(umaString + ' em um lindo dia.');
+console.log(`${umaString} em um lindo dia.`); 
