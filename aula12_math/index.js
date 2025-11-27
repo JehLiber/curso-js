1e5e3b2700cd13dec24a81ef52545463b967b0f7
Jessica Liberato <100487905+JehLiber@users.noreply.github.com>
2025-11-27 20:37:16 -0300

    Math no JS
    atualização sobre a aula 12, trabalhando com Math

---
+++ aula12_math/index.js
@@ -0,0 +1,24 @@
+//existem várias funções com Math.
+
+
+//let num1 = 9;
+//console.log(num1 ** (1/2)); //raiz quadrada de 9 = 3
+
+// console.log(!!(100 / 0)); // = True
+
+//let num1 = 9.588988;
+
+// let num2 = Math.floor(num1); //ele arredonda para menos  = 9
+// let num2 = Math.ceil(num1); //ele arredonda para mais  = 10
+//let num2 = Math.round(num1); //ele arredonda dependendo do número do decimal = adima de .50 ele arredonda para +, se for .49 arredonda para -
+//console.log(num2);
+
+//console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6,5)); //vai mostrar o número maior da sequencia = 1500
+//console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6,5)); //vai mostrar o número menor da sequencia = -50
+
+// console.log(Math.random()); // aparece um número aleatório menor que 1
+
+// Math.PI
+
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5); // número aleatório entre 10 e 5  
+//console.log(aleatorio)
